548bea1b0389117f96db924c0cb54207dffe670c
maxime.louis <maxam33@hotmail.fr>
2016-02-26 21:43:22 +0000

    Ajout d'un fichier pour le machine learning.

--- machine_learning.py
+++ machine_learning.py
@@ -10,3 +10,11 @@ import random as rd
 from Tkinter import *
 import Puissance4.py
 
+#On associe à chaque état + coup une récompense R(état)
+#On apprendre R(état) + gamma*R(meilleur tat suivant) + j^2 R(meilleur etat suivant)...
+
+
+class ValueIteration:
+    def __init__():
+        self.v = [[rd.random(1.)]*Puissance4.longueur] * Puissance4.largeur
+    
