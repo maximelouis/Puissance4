a483b6c1a831162a68d38446426447e80ddd68e6
thomas <thomas>
2016-02-26 22:04:53 +0000

    class IA, IA niveau 1

--- P4_IA.py
+++ P4_IA.py
@@ -7,3 +7,18 @@ Created on Fri Feb 26 21:27:56 2016
 
 # IA du Puissance4
 
+import Puissance4.py
+
+# Pour les 7 coups possibles j'obtient la note récursivement
+
+class P4IA:
+    
+    
+    
+    def jouerIA(self,p,joueur):
+        for i in range(Puissance4.longueur):
+            if p.jouer(i,joueur):
+                return i
+            p.retirer(i,joueur)
+            
+            
