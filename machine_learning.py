23bbacef42e4a049cb260a455deaaee71d555779
maxime.louis <maxam33@hotmail.fr>
2016-02-27 00:01:16 +0000

    Debut de la classe Genetic qui sélectionne les bons réseaux de neurones en les entrainant successivement sur différents situations.

--- machine_learning.py
+++ machine_learning.py
@@ -10,11 +10,46 @@ import random as rd
 from Tkinter import *
 import Puissance4.py
 
-#On associe à chaque état + coup une récompense R(état)
-#On apprendre R(état) + gamma*R(meilleur tat suivant) + j^2 R(meilleur etat suivant)...
+from sklearn import metrics
+from sknn.mlp import Regressor, Layer
+from sknn.backend import lasagne
+import pickle
 
 
-class ValueIteration:
-    def __init__():
-        self.v = [[rd.random(1.)]*Puissance4.longueur] * Puissance4.largeur
+
+class Genetic:
     
+    num = Puissance4.longueur * 2
+    
+    def __init__(self):
+        self.nns = []#Current neural networks to be evaluated or mutated
+        for i in xrange(Genetic.num):
+            self.nns.append(Regressor(layers=[Layer("Sigmoid", units=(Puissance4.longueur * Puissance4.largeur * 2)),Layer("Sigmoid", units=100), Layer("Sigmoid")], learning_rate=0.01, n_iter=1))
+
+
+    def __init__(self, filename):
+        self.nns = []
+        for i in xrange(Genetic.num):
+            s = s+str(i)
+            self.nns.append(pickle.load(open(s, 'rb')))
+            s = ""
+    
+
+    def select_best(self):#Select the 2 bests nns in the list by testing against an AI, discard the others
+
+
+    def mutate(self):#Mutate the neural nets we kept
+    #Generate 2 different situations
+    a = Puissance4()
+    #CALL THE IA TO PLAY A COUPLE TIMES AND GET THE SITUATION
+    
+    #Fit the neural networks to those situations
+    
+    def save():#Saves the current best networks
+        for i,elt in enumerate(self.nns):
+            s = 'nn{}.pkl'.format{i}
+            pickle.dump(elt, open('nn{}.pkl', 'wb'))
+
+
+                            
+                            
